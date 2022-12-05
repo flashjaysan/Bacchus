0324d925d8c6b6851a0160aaa794ee011c328844
flashjaysan <jaysan@wanadoo.fr>
2022-12-05 13:27:04 +0100

    Création du factory de contexte de base de données.

---
+++ Bacchus.DataAccess/DbContextFactory/BacchusDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Bacchus.DataAccess.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Bacchus.DataAccess.DbContextFactory;
+
+public class BacchusDbContextFactory : IDesignTimeDbContextFactory<BacchusDbContext>
+{
+    readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json")
+       .Build();
+
+    public BacchusDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<BacchusDbContext> builder = new DbContextOptionsBuilder<BacchusDbContext>();
+        builder.UseSqlServer(_configuration.GetConnectionString("BacchusDb"), builder => builder.MigrationsAssembly("Bacchus.DataAccess"));
+        return new BacchusDbContext(builder.Options);
+    }
+}

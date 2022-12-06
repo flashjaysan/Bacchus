e48eeda5120dacb88c12586521498b1e029b8c46
flashjaysan <jaysan@wanadoo.fr>
2022-12-06 11:09:25 +0100

    Création de contrôleurs vides pour chaque modèle de données.

---
+++ Bacchus.API/Controllers/SupplierController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bacchus.API.Controllers;
+
+public class SupplierController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Bacchus.API/Controllers/WineController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bacchus.API.Controllers;
+
+public class WineController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
